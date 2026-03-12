3065179c59eb06fbed63975e9648d1c6cda7f28e
thomasenuud-oss <thomasenuud@gmail.com>
2026-03-12 15:06:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -783,4 +783,17 @@
       <!--====== Main js ======-->
       <script src="js/main.js"></script>
    </body>
+   <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/69b284f19140081c3676dae0/1jjglekpi';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </html>
